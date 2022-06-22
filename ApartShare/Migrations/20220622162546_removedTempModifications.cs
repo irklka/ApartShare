390cbd237e5be00e8adad05dcc73cccd8220217d
kapitan <62106615+irklka@users.noreply.github.com>
2022-06-22 20:30:49 +0400

    added commented code to migrate from storing base64 to bytearray representation

---
+++ ApartShare/Migrations/20220622162546_removedTempModifications.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApartShare.Migrations
+{
+    public partial class removedTempModifications : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageBase64ByteArray",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ImageBase64ByteArray",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+    }
+}
--- ApartShare/Models/Extensions/ExtensionMappers.cs
+++ ApartShare/Models/Extensions/ExtensionMappers.cs
@@ -2,6 +2,7 @@
 using ApartShare.Models.DTOs.ApartmentDTOs;
 using ApartShare.Models.DTOs.RequestDtos;
 using ApartShare.Models.DTOs.UserDtos;
+using System.Text;
 
 namespace ApartShare.Models.Extensions
 {
@@ -17,11 +18,12 @@ namespace ApartShare.Models.Extensions
                 LoginName = user.LoginName,
                 Password = user.Password,
                 MyApartment = user.MyApartment.ToDTO(),
-                ImageBase64 = user.ImageBase64,
+                ImageBase64 = user.ImageBase64
+                //ImageBase64 = Base64BytesToString(user.ImageBase64ByteArray),
             };
         }
 
-         public static UserApartmentDTO? ToDTO(this Apartment apartment)
+        public static UserApartmentDTO? ToDTO(this Apartment apartment)
         {
             if( apartment == null)
             {
@@ -53,5 +55,21 @@ namespace ApartShare.Models.Extensions
                 HostId = request.HostId
             };
         }
+
+        //private static string Base64BytesToString(byte[]? imageBase64ByteArray)
+        //{
+        //    var result = new StringBuilder("data:image/png;base64,");
+
+        //    if (imageBase64ByteArray == null)
+        //    {
+        //        return null;
+        //    }
+        //    else
+        //    {
+        //        var base64 = Convert.ToBase64String(imageBase64ByteArray);
+        //        result.Append(base64);
+        //        return result.ToString();
+        //    }
+        //}
     }
 }
